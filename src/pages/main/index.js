cc2788bbeab674bc19bd20f4c66df782425146a7
songlairui <cnre@live.cn>
2018-10-18 02:34:55 +0800

    增加授权按钮，增加Toast提示

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,26 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View } from "@tarojs/components";
-import { AtButton, AtCard, AtList, AtListItem, AtProgress } from "taro-ui";
+import { View, Button } from "@tarojs/components";
+import {
+  AtButton,
+  AtCard,
+  AtList,
+  AtListItem,
+  AtProgress,
+  AtToast
+} from "taro-ui";
 
 import "./index.less";
 
 class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      canIUse: Taro.canIUse("button.open-type.getUserInfo"),
+      toast: {
+        visible: false,
+        text: ""
+      }
+    };
   }
   config = {
     navigationBarTitleText: "MAIN"
@@ -30,9 +43,27 @@ class Index extends Component {
       url: target
     });
   }
+  handleGetUserInfo(e) {
+    const userInfo = e.detail.userInfo;
+    if (!userInfo) {
+      this.setState({ toast: { visible: true, text: "授权后才能参加活动" } });
+    }
+  }
+  handleClose = () => {
+    console.warn("close");
+    this.setState({
+      toast: { visible: false }
+    });
+  };
   render() {
     return (
       <View className="wrapper">
+        <AtToast
+          isOpened={this.state.toast.visible}
+          text={this.state.toast.text}
+          status="error"
+          onClose={this.handleClose}
+        />
         <View className="userBar">
           <View className="figure">
             <View className="avatarPlace">
@@ -41,6 +72,18 @@ class Index extends Component {
           </View>
           <View className="nick-name">
             <open-data type="userNickName" />
+          </View>
+          <View className="auth">
+            {this.state.canIUse ? (
+              <Button
+                open-type="getUserInfo"
+                onGetUserInfo={this.handleGetUserInfo}
+              >
+                授权登录
+              </Button>
+            ) : (
+              <View>请升级微信版本</View>
+            )}
           </View>
         </View>
         <View className="main">
